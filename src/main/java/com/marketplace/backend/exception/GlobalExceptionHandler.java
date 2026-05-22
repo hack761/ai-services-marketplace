e31e56d86ac835739f209e58d4ac215d7946a45e
hack761 <snehithspeaks@gmail.com>
2026-05-23 01:48:02 +0530

    Added validation and global exception handling

--- src/main/java/com/marketplace/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/marketplace/backend/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.marketplace.backend.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,4 +17,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleEmailExists(EmailAlreadyExistsException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+
+        Map<String,String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
